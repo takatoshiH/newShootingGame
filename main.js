1eeec6e21b056b6cf45b79cb0a56ade297b9285e
takatoshiH <t.hikijiri@gmail.com>
2020-03-04 01:51:57 +0900

    機能追加

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 class Bullet {
   constructor() {
+    this.bullet = document.createElement("div");
+    this.bullet.classList.add("bullet");
+    this.bullet.style.top = "400px";
+    this.bullet.style.left = "50%";
+    document.getElementById("gameSpace").appendChild(this.bullet);
 
+    this.intervalId = setInterval(() => {
+      if (parseInt(this.bullet.style.top) <= 10) {
+        clearInterval(this.intervalId);
+        document.getElementById("gameSpace").removeChild(this.bullet);
+      }
+      this.bullet.style.top = String(parseInt(this.bullet.style.top) - 10) + "px";
+      console.log("ok");
+    }, 10);
   }
 }
 
@@ -8,14 +21,26 @@ class Target {
   constructor() {
     this.target = document.createElement("div");
     this.target.classList.add("target");
-    this.target.style.top = String(Math.floor(Math.random() * 100)) + "px";
+    this.speed = Math.floor(Math.random() * 50) + 10;
+    this.target.style.top = String(Math.floor(Math.random() * 250)) + "px";
     this.target.style.left = "0px";
     document.getElementById("gameSpace").appendChild(this.target);
 
     this.intervalId = setInterval(() => {
-      this.target.style.left = String(parseInt(this.target.style.left) + 10) + "px";
-    }, 1000);
+      if (parseInt(this.target.style.left) >= window.innerWidth) {
+        clearInterval(this.intervalId);
+      }
+      this.target.style.left = String(parseInt(this.target.style.left) + this.speed) + "px";
+    }, 100);
   }
 }
 
-new Target();
+const intervalId = setInterval(() => {
+  new Target();
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+}, 10000);
+
+new Bullet();
